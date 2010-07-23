ac0cf86f2b7f4c24567749939b1685499a871d98
GreYFoXGTi <GreYFoXGTi@GmaiL.CoM>
2010-07-24 02:12:48 +0800

    changed naming from DD.MM.YYYY to YYYY.MM.DD for better file sorting
    Signed-off-by: GreYFoXGTi <GreYFoXGTi@Gmail.com>

--- src/game/version.hpp
+++ src/game/version.hpp
@@ -1,6 +1,6 @@
 /* copyright (c) 2007 magnus auvinen, see licence.txt for more info */
 #include "generated/nethash.c"
 #define GAME_VERSION "0.5.2"
-#define RACE_VERSION "04.02.2010 Edited by: Fluxid, LemonFace, noother & GreYFoX@GTi 23.07.2010"
+#define RACE_VERSION "04.02.2010 Edited by: Fluxid, LemonFace, noother & GreYFoX@GTi 2010.07.23"
 #define GAME_NETVERSION "0.5 b67d1f1a1eea234e"
 
