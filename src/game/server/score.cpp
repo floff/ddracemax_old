4ffb357870e581152bfe14484c90956a331fc4da
noother <noothy@gmail.com>
2010-08-21 10:57:16 +0800

    scores now get loaded again before any access to it. This enables us to run many servers with the same maps and same record files without screwing up the scoring.

--- src/game/server/score.cpp
+++ src/game/server/score.cpp
@@ -62,6 +62,7 @@ void SCORE::load()
 
 PLAYER_SCORE *SCORE::search_name(const char *name, int &pos)
 {
+	load();
 	pos=0;
 	for (std::list<PLAYER_SCORE>::iterator i = top.begin(); i!=top.end(); i++)
 	{
@@ -77,6 +78,7 @@ PLAYER_SCORE *SCORE::search_name(const char *name, int &pos)
 
 PLAYER_SCORE *SCORE::search_name(const char *name)
 {
+	load();
 	for (std::list<PLAYER_SCORE>::iterator i = top.begin(); i!=top.end(); i++)
 	{
 		if (!strcmp(i->name, name))
@@ -88,6 +90,7 @@ PLAYER_SCORE *SCORE::search_name(const char *name)
 }
 
 PLAYER_SCORE *SCORE::get_rank(int rank) {
+	load();
 	rank--;
 	int c=0;
 	
@@ -104,6 +107,7 @@ PLAYER_SCORE *SCORE::get_rank(int rank) {
 
 void SCORE::parsePlayer(const char *name, float score)
 {
+	load();
 	PLAYER_SCORE *player=search_name(name);
 	if (player)
 	{
@@ -124,6 +128,7 @@ void SCORE::parsePlayer(const char *name, float score)
 
 void SCORE::top5_draw(int id, int debut)
 {
+	load();
 	int pos = 1;
 	//char buf[512];
 	game.send_chat_target(id, "----------- Top 5 -----------");
